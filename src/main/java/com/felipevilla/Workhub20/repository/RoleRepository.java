3376fb6e91eb659543bb897d8b570f7097717df3
Felipe villa <andresfelipevillapardo7@gmail.com>
2025-02-21 22:06:33 -0500

    Se añadió un nuevo metodo para encontrar el Rol User

--- src/main/java/com/felipevilla/Workhub20/repository/RoleRepository.java
+++ src/main/java/com/felipevilla/Workhub20/repository/RoleRepository.java
@@ -1,13 +1,20 @@
 package com.felipevilla.Workhub20.repository;
 
 import java.util.List;
+import java.util.Optional;
+
+
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.felipevilla.Workhub20.model.RoleModel;
+import com.felipevilla.Workhub20.model.Enum.RoleEnum;
+
 
 public interface RoleRepository extends CrudRepository<RoleModel, Long> {
    
     List<RoleModel> findRoleModelsByRoleEnumIn (List<String> names);
+
+    Optional<RoleModel> findByRoleEnum (RoleEnum USER);
 
 }
