6d0638cf6b61d198c32092bc35b789579232b46e
Felipe villa <andresfelipevillapardo7@gmail.com>
2025-02-21 22:01:21 -0500

    Implementación del endpoint para creación de admins

--- src/main/java/com/felipevilla/Workhub20/controller/AuthenticationController.java
+++ src/main/java/com/felipevilla/Workhub20/controller/AuthenticationController.java
@@ -3,6 +3,8 @@ package com.felipevilla.Workhub20.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.felipevilla.Workhub20.dto.AuthCreateUserRequest;
 import com.felipevilla.Workhub20.dto.AuthLoginRequest;
 import com.felipevilla.Workhub20.dto.AuthResponse;
+import com.felipevilla.Workhub20.dto.AuthResponseUpdate;
+import com.felipevilla.Workhub20.dto.AuthUpdateUserRolesRequest;
 import com.felipevilla.Workhub20.service.UserDetailsService;
 
 import jakarta.validation.Valid;
@@ -32,4 +36,11 @@ public class AuthenticationController {
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthLoginRequest userRequest){
         return new ResponseEntity<>(this.userDetailsService.loginUser(userRequest), HttpStatus.OK); 
     }
+
+    @PatchMapping("/updateRoles")
+    public ResponseEntity<AuthResponseUpdate> updateUserRoles(@RequestBody AuthUpdateUserRolesRequest authUpdateUserRolesRequest){ 
+        return new ResponseEntity<>(this.userDetailsService.updateUserRoles(authUpdateUserRolesRequest), HttpStatus.OK);
+    
+    }   
+ 
 }
